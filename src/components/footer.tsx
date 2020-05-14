b80148d4e6eb2ffbfb8d3f2515f5d11858529220
hayatasatomi <lionvibe.14@gmail.com>
2020-05-14 19:07:06 +0900

    Footer 適用

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -25,15 +25,11 @@ const Container = styled.footer`
   height: 60px;
   line-height: 60px;
   margin-top: 140px;
-  font-family: Roboto-Light;
-  font-size: 16px;
-  color: #A0A0A0;
-  letter-spacing: 0.5px;
   text-align: center;
 `;
 
 const HR = styled.hr`
-  width: 70%;
+  width: 60%;
   margin-top: 65px;
   margin-bottom: 65px;
   border: 0.5px solid #979797;
