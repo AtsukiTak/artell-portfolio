61993b08256e2ba3c067693dffb8a51376830f28
AtsukiTak <takatomgoo@gmail.com>
2021-03-18 15:49:49 +0900

    serviceAccountのシークレットを環境変数から読み込む

--- src/server-libs/firebase.ts
+++ src/server-libs/firebase.ts
@@ -2,7 +2,12 @@ import firebase from "firebase/app";
 import admin from "firebase-admin";
 
 import { getFirebaseApp } from "../libs/firebase";
-import serviceAccount from "../../serviceAccountKey.json";
+
+const serviceAccountJson = process.env.SERVICE_ACCOUNT_JSON;
+if (!serviceAccountJson) {
+  throw new Error("SERVICE_ACCOUNT_JSON is not set!!!");
+}
+const serviceAccount = JSON.parse(serviceAccountJson);
 
 export const getFirebase = (): firebase.app.App => {
   return getFirebaseApp();
