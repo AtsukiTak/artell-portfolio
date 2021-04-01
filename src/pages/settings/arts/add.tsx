7b90dba18e113992ee46924fe5f029429614c17b
AtsukiTak <takatomgoo@gmail.com>
2021-04-01 23:15:06 +0900

    作品追加後に作品一覧ページにリダイレクトする

--- src/pages/settings/arts/add.tsx
+++ src/pages/settings/arts/add.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { useRouter } from "next/router";
+
 import EditArtTemplate from "components/templates/settings/arts/add";
 import { createArtRequest } from "libs/apis/art/create";
 
 const EditArtPage: React.FC = () => {
+  const router = useRouter();
+
   const onSubmit = React.useCallback(
-    (data) => createArtRequest(data).then(() => undefined),
-    []
+    (data) =>
+      createArtRequest(data)
+        .then(() => {
+          alert("新しい作品を追加しました！");
+          router.push("/settings/arts");
+        })
+        .catch(() => {
+          alert(
+            "作品の登録に失敗しました。お手数ですが運営までお問い合わせください。"
+          );
+        }),
+    [router]
   );
 
   return <EditArtTemplate onSubmit={onSubmit} />;
