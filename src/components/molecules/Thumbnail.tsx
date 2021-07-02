67f70bbd224e9130ce4024d8b6eb25ee5732952e
AtsukiTak <takatomgoo@gmail.com>
2021-07-02 19:15:50 +0900

    Thumbnailコンポーネントの修正。正方形以外の画像も適切に表示できるように

--- src/components/molecules/Thumbnail.tsx
+++ src/components/molecules/Thumbnail.tsx
@@ -1,10 +1,29 @@
+import React from "react";
 import styled from "styled-components";
 
-import { WidthBasedSquare } from "components/molecules/Square";
+type Props = {
+  src: string;
+};
 
-export const Thumbnail = styled(WidthBasedSquare)<{ src: string }>`
-  background-image: url(${(props) => props.src});
-  background-size: cover;
+export const Thumbnail: React.FC<Props> = ({ src }) => (
+  <SquareContainer>
+    <ImageContainer style={{ backgroundImage: `url(${src})` }} />
+  </SquareContainer>
+);
+
+// `padding-bottom: 100%` はwidthと同じ分buttomにpaddingをとる
+const SquareContainer = styled.div`
+  position: relative;
+  width: 100%;
+  padding-bottom: 100%;
+`;
+
+const ImageContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-size: contain;
+  background-repeat: no-repeat;
   background-position: center;
 `;
 
