6f6d27aaf5c2daebb6cdf249e223bfb2b945cffc
AtsukiTak <takatomgoo@gmail.com>
2021-11-21 13:33:24 +0900

    update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,9 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-declare module "*.svg" {
-  /* eslint @typescript-eslint/no-explicit-any: off */
-  const content: any;
-  export default content;
-
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
